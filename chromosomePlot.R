5a04cc4d9c7926b26c12f90e45aff31d680f939e
Caiden <ckumar@neb.com>
2024-09-25 16:09:02 -0400

    Edited R script

--- chromosomePlot.R
+++ chromosomePlot.R
@@ -23,6 +23,10 @@ chromsToPlot <- c("chr13", "chr14", "chr15", "chr16", "chr17", "chr18", "chr19",
 
 # chromsToPlot <- c("chr1", "chr2", "chr3", "chr4", "chr5", "chr6", "chr7", "chr8", "chr9", "chr10", "chr11", "chr12")
 
+
+chromsToPlot_all <- c("chr1", "chr2", "chr3", "chr4", "chr5", "chr6", "chr7", "chr8", "chr9", "chr10", "chr11", "chr12", "chr13", "chr14", "chr15", "chr16", "chr17", "chr18", "chr19", "chr20", "chr21", "chr22", "chrX", "chrY")
+
+
 chromDataX1 = list()
 
 chromDataY1 = list()
